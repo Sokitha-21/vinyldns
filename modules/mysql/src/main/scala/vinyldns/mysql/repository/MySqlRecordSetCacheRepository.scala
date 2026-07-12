79fded1196b6d9cec14d271c985a19c8502d8c1a
Sokitha-21 <krishnansokitha@gmail.com>
2026-07-13 00:35:32 +0530

    code fixes

--- modules/mysql/src/main/scala/vinyldns/mysql/repository/MySqlRecordSetCacheRepository.scala
+++ modules/mysql/src/main/scala/vinyldns/mysql/repository/MySqlRecordSetCacheRepository.scala
@@ -407,6 +407,7 @@ class MySqlRecordSetCacheRepository
                   val setDelimiter = SQLSyntax.join(countOpts, sqls"AND")
                   sqls"WHERE".append(setDelimiter)
                 } else sqls""
+                
               val countGroupBy = sqls"""
                   GROUP BY recordset_data.recordset_id, recordset_data.type
                 ) AS grouped_count
